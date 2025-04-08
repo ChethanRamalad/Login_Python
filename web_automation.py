917de8dc2279b0b9e0fe0046e37e8694db206875
chethan <cramalad@gmail.com>
2025-04-08 15:01:10 +0530

    Login_Positive

--- web_automation.py
+++ web_automation.py
@@ -5,13 +5,13 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 driver.get("https://practicetestautomation.com/practice-test-login/")
 
-element = driver.find_element(By.XPATH, "/html/body/div/div/section/section/div[1]/div[1]/input")
+element = driver.find_element(By.ID, "username")
 element.send_keys("student")
 
-element = driver.find_element(By.XPATH, "/html/body/div/div/section/section/div[1]/div[2]/input")
+element = driver.find_element(By.ID, "password")
 element.send_keys("Password123")
 
-element = driver.find_element(By.XPATH, "/html/body/div/div/section/section/div[1]/button")
+element = driver.find_element(By.XPATH, "submit")
 element.click() 
 
 time.sleep(10)
